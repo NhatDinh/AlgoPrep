6c9734eed3cdd1decf0537d89554bd42707b1013
NhatDinh <nhat.ldinh@gmail.com>
2019-06-09 15:38:54 -0700

    o(n) solution for reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,32 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  var sign = getSign(n);
+  var reversed = reverseConst(n.toString());
+  var reversedInt = getInt(reversed) * sign;
+  return reversedInt;
+}
+
+//O(n) reverse function
+function reverseConst(str) {
+  var reversed = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    var char = str[i];
+    reversed += char;
+  }
+  return reversed;
+}
+
+//utilities
+function getInt(str) {
+  var integer = parseInt(str);
+  return integer;
+}
+
+function getSign(int) {
+  var sign = Math.sign(int);
+  return sign;
+}
 
 module.exports = reverseInt;
